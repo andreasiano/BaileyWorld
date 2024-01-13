77bfb33ddb3e00477909ca1e133f260f93bd0eca
AndreaPensieri <andrejazz2011@gmail.com>
2024-01-13 23:02:20 +0100

    Push

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,11 +1,24 @@
 import React, { createContext, ReactNode, useContext, useState } from 'react';
 
-interface ICartContext {
-  product: any[];
-  addToCart: (cart: any) => void;
-  removeFromCart: (cart: any) => void;
-  
+interface IProduct {
+  img: string;
+  name: string;
+  price: number;
 }
+
+interface ICartContext {
+  product: IProduct[];
+  addToCart: (cart: IProduct) => void;
+  removeFromCart: (cart: IProduct) => void;
+}
+
+const areProductsEqual = (product1: IProduct, product2: IProduct) => {
+  return (
+    product1.img === product2.img &&
+    product1.name === product2.name &&
+    product1.price === product2.price
+  );
+};
 
 const CartContext = createContext<ICartContext>({
   product: [],
@@ -18,31 +31,21 @@ interface ICartContextProvider {
 }
 
 export const CartContextProvider: React.FC<ICartContextProvider> = ({ children }) => {
-  const [product, setProduct] = useState<any>([]);
+  const [product, setProduct] = useState<IProduct[]>([]);
 
-  const addToCart = (cart: any) => {
-
+  const addToCart = (cart: IProduct) => {
     const isProductInCart = product.some((item) => areProductsEqual(item, cart));
 
     if (!isProductInCart) {
-      setProduct((prevProducts: any) => [...prevProducts, cart]);
+      setProduct((prevProducts) => [...prevProducts, cart]);
     } else {
       console.warn('Product is already in the cart');
     }
   };
 
-
-  const removeFromCart = (productToRemove: any) => {
-    setProduct((prevProducts: any) =>
-      prevProducts.filter((product: any) => !areProductsEqual(product, productToRemove))
-    );
-  };
-  
-  const areProductsEqual = (product1: any, product2: any) => {
-    return (
-      product1.img === product2.img &&
-      product1.name === product2.name &&
-      product1.price === product2.price
+  const removeFromCart = (productToRemove: IProduct) => {
+    setProduct((prevProducts) =>
+      prevProducts.filter((product) => !areProductsEqual(product, productToRemove))
     );
   };
 
